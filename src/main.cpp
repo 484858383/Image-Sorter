2fdaae908a0bb929a50ffc06b05c818cb8b85f47
Ronnie Kaczynski <ronniie.k@gmail.com>
2018-12-15 14:39:10 +0000

    image sorting works

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,56 @@
 #include<SFML/Graphics.hpp>
 #include<iostream>
+#include<vector>
+#include<string>
 
-constexpr int width = 512;
+constexpr int width = 1024;
 constexpr int height = 512;
 
 int main()
 {
-	std::cout << "g\n";
-
-
+	std::string fileName = "greyscale.png";
 	sf::RenderWindow window(sf::VideoMode(width, height), "Image Sorter", sf::Style::Close);
 
+	sf::Image before;
+	sf::Image after;
+
+	sf::Texture tBefore;
+	sf::Texture tAfter;
+
+	before.loadFromFile(fileName);
+	auto size = before.getSize();
+	auto index = [&](int x, int y) {return x + y * size.x; };
+
+	std::vector<sf::Uint32> colorValues;
+	colorValues.reserve(size.x * size.y);
+
+	after.create(size.x, size.y, sf::Color::Black);
+
+	for(int y = 0; y < size.y; y++)
+	for(int x = 0; x < size.x; x++)
+		colorValues.push_back(before.getPixel(x, y).toInteger());
+
+	std::sort(colorValues.rbegin(), colorValues.rend());
+	//for(auto& i : colorValues)
+	//	std::cout << i << std::endl;
+
+	for(int y = 0; y < size.y; y++)
+	for(int x = 0; x < size.x; x++)
+	{
+		auto color = colorValues[index(x, y)];
+		after.setPixel(x, y, sf::Color(color));
+	}
+
+	tBefore.loadFromImage(before);
+	tAfter.loadFromImage(after);
+
+	sf::RectangleShape quadBefore({width / 2, static_cast<float>(height)});
+	quadBefore.setTexture(&tBefore);
+
+	sf::RectangleShape quadAfter({width / 2, static_cast<float>(height)});
+	quadAfter.setPosition(width / 2, 0);
+	quadAfter.setTexture(&tAfter);
+			
 
 	while(window.isOpen())
 	{
@@ -26,10 +66,13 @@ int main()
 
 		window.clear();
 
+		window.draw(quadBefore);
+		window.draw(quadAfter);
 
 		window.display();
 	}
 
+	std::cout << "Press any key to continue...\n";
 	std::cin.get();
 	return 0;
 }
